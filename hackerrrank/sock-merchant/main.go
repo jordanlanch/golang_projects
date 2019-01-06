b8c88c3fa92f47df458c7c4c7d3d0ff69c290daf
Jordan Lancheros <Jordan1703@hotmail.es>
2019-01-06 16:20:18 -0500

    adicion ejercicios Hackerrank

---
+++ hackerrrank/sock-merchant/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the sockMerchant function below.
+func sockMerchant(n int32, ar []int32) int32 {
+	var pairs int32
+	m := make(map[int32]int)
+	for i := range ar {
+		if _, exist := m[ar[i]]; exist {
+			m[ar[i]] = m[ar[i]] + 1
+		} else {
+			m[ar[i]] = 1
+		}
+	}
+
+	for _, value := range m {
+		if value > 1 {
+			pairs = pairs + int32(value/2)
+		}
+
+	}
+
+	return pairs
+
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arTemp := strings.Split(readLine(reader), " ")
+
+	var ar []int32
+
+	for i := 0; i < int(n); i++ {
+		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		arItem := int32(arItemTemp)
+		ar = append(ar, arItem)
+	}
+
+	result := sockMerchant(n, ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
